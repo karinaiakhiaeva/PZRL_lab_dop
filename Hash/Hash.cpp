3d926482b9f611432929d2a0d4841390eea406de
karinaiakhiaeva <karina.iakhiaeva7@gmail.com>
2026-05-31 11:35:38 +0300

    Update Hash.cpp

--- Hash/Hash.cpp
+++ Hash/Hash.cpp
@@ -3,31 +3,29 @@
 #include <algorithm>
 #include <stdexcept>
 
-// Конструктор
+
 HashTable::HashTable(size_t size) noexcept 
     : _capacity(static_cast<int32_t>(size)), _filled(0) {
     table.resize(_capacity);
 }
 
-// Деструктор
 HashTable::~HashTable() {
     // vector и list сами очистят память
 }
 
-// Хеш-функция
+
 size_t HashTable::hash_function(const KeyType &key) const {
     return std::hash<KeyType>{}(key) % _capacity;
 }
 
-// Вставка элемента
+
 void HashTable::insert(const KeyType &key, const ValueType &value) {
-    // Проверяем необходимость расширения
+    
     if (getLoadFactor() > 0.75) {
         // Расширяем таблицу в 2 раза
         int32_t new_capacity = _capacity * 2;
         std::vector<std::list<std::pair<KeyType, ValueType>>> new_table(new_capacity);
         
-        // Перехешируем все элементы
         for (const auto& bucket : table) {
             for (const auto& pair : bucket) {
                 size_t new_index = std::hash<KeyType>{}(pair.first) % new_capacity;
@@ -41,20 +39,19 @@ void HashTable::insert(const KeyType &key, const ValueType &value) {
     
     size_t index = hash_function(key);
     
-    // Проверяем, существует ли уже такой ключ
+    
     for (auto& pair : table[index]) {
         if (pair.first == key) {
-            pair.second = value; // Обновляем значение
+            pair.second = value; 
             return;
         }
     }
     
-    // Если ключа нет, добавляем новую пару
     table[index].push_back({key, value});
     _filled++;
 }
 
-// Поиск элемента
+
 bool HashTable::find(const KeyType &key, ValueType &value) const {
     size_t index = hash_function(key);
     
@@ -68,7 +65,6 @@ bool HashTable::find(const KeyType &key, ValueType &value) const {
     return false;
 }
 
-// Удаление элемента
 void HashTable::remove(const KeyType &key) {
     size_t index = hash_function(key);
     auto& bucket = table[index];
@@ -82,7 +78,6 @@ void HashTable::remove(const KeyType &key) {
     }
 }
 
-// Оператор доступа по ключу
 ValueType& HashTable::operator[](const KeyType &key) {
     size_t index = hash_function(key);
     
@@ -92,13 +87,13 @@ ValueType& HashTable::operator[](const KeyType &key) {
         }
     }
     
-    // Если ключ не найден, вставляем новую пару со значением 0.0
+    
     table[index].push_back({key, 0.0});
     _filled++;
     return table[index].back().second;
 }
 
-// Получение загрузки таблицы
+
 double HashTable::getLoadFactor() {
     return static_cast<double>(_filled) / _capacity;
 }
